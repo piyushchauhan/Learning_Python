3719a30ec57570f212e79378b5c1b95bb0e7fd31
mmayankk <31548906+mmayankk@users.noreply.github.com>
2019-03-06 18:39:43 +0530

    Update Excel-to-CSV Converter.py

--- Excel-to-CSV Converter.py
+++ Excel-to-CSV Converter.py
@@ -1,11 +1,11 @@
 #! python3
 """
-Excel can save a spreadsheet to a CSV file with a few mouse clicks, but if you had to convert hundreds of Excel files
+Excel can save a spreadsheet to a CSV file w ith a few mouse clicks, but if you had to convert hundreds of Excel files
 to CSVs, it would take hours of clicking. Using the openpyxl module from Chapter 12, write a program that reads all the
 Excel files in the current working directory and outputs them as CSV files.
 A single Excel file might contain multiple sheets; you’ll have to create one CSV file per sheet. The filenames of the
 CSV files should be <excel filename>_<sheet title>.csv, where <excel filename> is the filename of the Excel file
-without the file extension (for example, 'spam_data', not 'spam_data.xlsx') and <sheet title> is the string from the
+without the file extension (for example, 'spam_data',  not 'spam_data.xlsx') and <sheet title> is the string from the
 Worksheet object’s title variable.
 
 This program will involve many nested for loops. The skeleton of the program will look something like this:
