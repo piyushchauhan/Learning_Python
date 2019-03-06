943afb9cd44d5c596361ad4bd3722992ffb7083e
mmayankk <31548906+mmayankk@users.noreply.github.com>
2019-03-06 18:31:16 +0530

    Update IsPhoneNumber.py

--- IsPhoneNumber.py
+++ IsPhoneNumber.py
@@ -21,7 +21,7 @@ print('415-555-424322 is a phone number:')
 print(isPhoneNumber('415-555-424322'))
 print('Moshi moshi is a phone number:')
 print(isPhoneNumber('Moshi moshi'))
-message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+message =  'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
 print('message is ')
 print(message)
 for i in range(len(message)):
